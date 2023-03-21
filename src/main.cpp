0c1b2b73d950c9c47f88d01105d9f645a1722ed3
destroY17 <105844351+destroY17@users.noreply.github.com>
2023-03-21 21:38:13 +0300

    Code style fix

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,38 @@
 #include <iostream>
 
 int main() {
-	try {
-		Automata automata({ "Latte", "Glace", "Cappuccino", "Espresso" }, { 100, 80, 90, 75 });
-		std::cout << "Automata is running...\n";
-		automata.on();
-		std::cout << "Automata state is " + automata.getState() + "\n";
-		std::cout << "Menu:\n";
-		std::cout << automata.getMenu();
-		int coins = 100;
-		automata.coin(coins);
-		std::cout << coins << " coins accepted" << std::endl;
-		std::cout << "Automata state is " << automata.getState() << std::endl;
-		int drinkIndex = 3;
-		automata.choice(drinkIndex);
-		std::cout << drinkIndex << " item selected and cooking has started" << std::endl;
-		std::cout << "Cooking is finish. Automata is " << automata.getState() << std::endl;
-		coins = 30;
-		automata.coin(coins);
-		std::cout << coins << " coins accepted" << std::endl;
-		std::cout << "Automata state is " << automata.getState() << std::endl;
-		automata.cancel();
-		std::cout << "Cancel operation. Automata state is " << automata.getState() << std::endl;
-		std::cout << "Automata turns off..." << std::endl;
-		automata.off();
-		std::cout << "Automata state is " << automata.getState() << std::endl;
-	}
-	catch (std::exception ex) {
-		std::cout << ex.what();
-	}
+    try {
+        std::vector<std::string> menu = { "Latte", "Glace",
+            "Cappuccino", "Espresso" };
+        std::vector<int> prices = { 100, 80, 90, 75 };
+        Automata automata(menu, prices);
+        std::cout << "Automata is running...\n";
+        automata.on();
+        std::cout << "Automata is " << automata.getState() << std::endl;
+        std::cout << "Menu:" << std::endl;
+        std::cout << automata.getMenu();
+        int coins = 100;
+        automata.coin(coins);
+        std::cout << coins << " coins accepted" << std::endl;
+        std::cout << "Automata is " << automata.getState() << std::endl;
+        int drinkIndex = 3;
+        automata.choice(drinkIndex);
+        std::cout << drinkIndex << " item selected and";
+        std::cout << " cooking has started" << std::endl;
+        std::cout << "Cooking is finish.";
+        std::cout << "Automata is " << automata.getState() << std::endl;
+        coins = 30;
+        automata.coin(coins);
+        std::cout << coins << " coins accepted" << std::endl;
+        std::cout << "Automata is " << automata.getState() << std::endl;
+        automata.cancel();
+        std::cout << "Cancel operation.";
+        std::cout << "Automata is " << automata.getState() << std::endl;
+        std::cout << "Automata turns off..." << std::endl;
+        automata.off();
+        std::cout << "Automata is " << automata.getState() << std::endl;
+    }
+    catch (std::exception ex) {
+        std::cout << ex.what();
+    }
 }
