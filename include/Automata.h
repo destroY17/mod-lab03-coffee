d22c124bd78a2805de69748597b14404b579fae4
destroY17 <105844351+destroY17@users.noreply.github.com>
2023-03-21 17:16:25 +0300

    Create Automata.h

---
+++ include/Automata.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <vector>
+#include <string>
+
+class Automata {
+public:
+	Automata(std::vector<std::string> menu, std::vector<int> prices);
+	void on();
+	void off();
+	void coin(int sum);
+	std::string getMenu();
+	std::string getState();
+	void choice(int drinkIndex);
+	void cancel();
+
+private:
+	void check(int drinkIndex);
+	void cook(int drinkIndex);
+	void finish();
+
+private:
+	enum STATES { OFF, WAIT, ACCEPT, CHECK, COOK };
+	int _cash;
+	std::vector<std::string> _menu;
+	std::vector<int> _prices;
+	STATES _state;
+};
